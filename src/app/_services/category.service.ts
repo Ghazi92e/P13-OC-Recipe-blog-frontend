994d8b36d071286224e68648225fe0fd65f8760d
Ghazi <ghaziatie@gmail.com>
2022-01-01 23:50:44 +0100

    add category service

---
+++ src/app/_services/category.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Category } from '../_models/Category.model';
+
+const url = 'http://127.0.0.1:8000/api-categories/'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) {}
+
+  getAllCategories() {
+    return this.http.get<Category[]>(url)
+  }
+
+  createCategory(data: any) {
+    return this.http.post(url, data);
+  }
+
+  delete(id: any) {
+    return this.http.delete(`${url}${id}`);
+  }
+}
