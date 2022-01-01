9f4224800db754ab53b78544b19ebe53565ff981
Ghazi <ghaziatie@gmail.com>
2022-01-01 23:49:11 +0100

    add Comment model

---
+++ src/app/_models/Comment.model.ts
@@ -0,0 +1,13 @@
+export class Comment {
+    id: number
+    created: string
+    description: string
+    user: number
+
+    constructor() {
+        this.id = 0
+        this.created = ''
+        this.description = ''
+        this.user = 0
+    }
+}
