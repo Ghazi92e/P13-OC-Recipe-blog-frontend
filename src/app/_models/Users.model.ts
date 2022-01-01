4ea5139850c8ead4a6d7facdddaf481d03dd7a6a
Ghazi <ghaziatie@gmail.com>
2022-01-01 23:49:41 +0100

    add Users model

---
+++ src/app/_models/Users.model.ts
@@ -0,0 +1,13 @@
+export class Users {
+    id: number
+    username: string
+    email: string
+    password: string
+
+    constructor() {
+        this.id = 0
+        this.username = ''
+        this.email = ''
+        this.password = ''
+    }
+}
