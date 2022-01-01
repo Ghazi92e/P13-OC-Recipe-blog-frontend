df4cf5e36a2324b20d06782c1c2ddb09c17e676c
Ghazi <ghaziatie@gmail.com>
2022-01-01 23:52:45 +0100

    add sign-in component

---
+++ src/app/authentication/sign-in/sign-in.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from '../../_services/users.service';
+import { Users } from '../../_models/Users.model';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent implements OnInit {
+
+  userForm: FormGroup | any
+  user: Users
+  currentuser: any
+  datatoken: any
+
+  constructor(private usersService: UsersService, private router: Router, private formBuilder: FormBuilder) 
+   { this.user = {
+    id: 0,
+    username: '',
+    password: '',
+    email: '',
+    }
+  }
+
+  ngOnInit(): void {
+    this.currentuser = localStorage.getItem('token');
+    this.initForm()
+    if (this.currentuser == null) {
+      console.log("je suis deco")
+    } else {
+      this.router.navigate(['recipes'])
+      console.log("je suis co")
+    }
+  }
+
+  initForm() {
+    this.userForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  getCurrentUser() {
+    const username = this.userForm?.get('username')?.value;
+    const password = this.userForm?.get('password')?.value;
+    this.user = new Users()
+    this.user.username = username
+    this.user.password = password
+    
+    this.usersService.getUserToken(this.user).subscribe( response => {
+      console.log(response);
+      this.datatoken = response
+      if (this.datatoken) {
+        localStorage.setItem('token', this.datatoken.token);
+        this.currentuser = localStorage.getItem('token');
+        console.log(this.currentuser)
+      }
+      
+      if (this.currentuser !== null) {
+        console.log("je suis connecté")
+        this.router.navigate(['recipes']).then(() => {
+          window.location.reload()
+        })
+      } else {
+        console.log("je ne suis pas connécté")
+      }
+      // this.router.navigate(['']);
+    },
+    error => {
+      console.log(error)
+      console.log(error.error.email)
+      console.log(error.error.username)
+    })
+  }
+
+}
