1f9d2c2867f90879667fc8b03f0c6505d7748a63
Ghazi <ghaziatie@gmail.com>
2022-01-01 23:51:03 +0100

    add comment service

---
+++ src/app/_services/comment.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Comment } from '../_models/Comment.model';
+
+const url = 'http://127.0.0.1:8000/api-comment/'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllComment() {
+    return this.http.get<Comment[]>(url)
+  }
+
+  createComment(data: any) {
+    return this.http.post(url, data);
+  }
+
+  deleteComment(id: any) {
+    return this.http.delete(`${url}${id}`);
+  }
+}
