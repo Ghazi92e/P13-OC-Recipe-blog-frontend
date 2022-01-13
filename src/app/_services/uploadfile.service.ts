f49ac0fd40111f764c71c8c9c7fbf96d35413dda
Ghazi <ghaziatie@gmail.com>
2022-01-13 11:09:33 +0100

    create uploadfile service

---
+++ src/app/_services/uploadfile.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FileUpload } from '../_models/Fileupload.model';
+
+const url = 'http://127.0.0.1:8000/api-upload-file/'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadfileService {
+
+  constructor(private http: HttpClient) { }
+
+  uploadFile(data: any) {
+    return this.http.post(url, data);
+  }
+
+  getFile() {
+    return this.http.get<FileUpload[]>(url)
+  }
+
+  getSingleFile(id: any) {
+    return this.http.get<FileUpload>(`${url}${id}`);
+  }
+}
