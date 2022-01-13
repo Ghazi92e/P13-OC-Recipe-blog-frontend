3760460d18caeefc4b4fbe6dd5dde0514eb84d36
Ghazi <ghaziatie@gmail.com>
2022-01-13 11:15:50 +0100

    add field file

--- src/app/recipes/single-recipe/single-recipe.component.ts
+++ src/app/recipes/single-recipe/single-recipe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FileUpload } from 'src/app/_models/Fileupload.model';
 import { Users } from 'src/app/_models/Users.model';
+import { UploadfileService } from 'src/app/_services/uploadfile.service';
 import { UsersService } from 'src/app/_services/users.service';
 import { Recipe } from '../../_models/Recipe.model';
 import { RecipesService } from '../../_services/recipes.service';
@@ -15,13 +17,21 @@ export class SingleRecipeComponent implements OnInit {
   recipe: Recipe
   currentuser: any
   user: Users[] = []
+  id = this.route.snapshot.params['id'];
+  fileupload: FileUpload
 
-  constructor(private recipesService: RecipesService, private route: ActivatedRoute, private router: Router, private userService: UsersService) { this.recipe = {id: 0, title:'', description: '', category: 0, file: '', user: 0} }
+  constructor(private recipesService: RecipesService, private route: ActivatedRoute, private router: Router, private userService: UsersService, private uploadFileService: UploadfileService) 
+  { this.recipe = {id: 0, title:'', description: '', category: 0, file: 0, user: 0}
+    this.fileupload = { id: 0, file: ''} }
 
   ngOnInit(): void {
-    this.recipesService.getSingleRecipe(this.route.snapshot.params.id).subscribe(data => {
+    this.recipesService.getSingleRecipe(this.id).subscribe(data => {
       this.recipe = data
       console.log(data)
+      this.uploadFileService.getSingleFile(this.recipe.file).subscribe(data => {
+        this.fileupload = data
+        console.log("je suis le single file" + data)
+      })
     })
 
     this.currentuser = localStorage.getItem('token');
@@ -30,6 +40,8 @@ export class SingleRecipeComponent implements OnInit {
       this.user = data
       console.log(this.user[0])
     })
+
+    console.log(this.recipe)
   }
 
   deleteRecipe(id: any) {
@@ -40,6 +52,10 @@ export class SingleRecipeComponent implements OnInit {
       error => {
         console.log(error)
     })
+  }
+
+  editRecipe() {
+    this.router.navigate(['/recipe', 'edit', this.id]);
   }
 
 }
