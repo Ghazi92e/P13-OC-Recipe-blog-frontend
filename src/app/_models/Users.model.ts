2de062092943f5d294da157faf36742b3f784dbc
Ghazi <ghaziatie@gmail.com>
2022-01-13 11:07:34 +0100

    add field file

--- src/app/_models/Users.model.ts
+++ src/app/_models/Users.model.ts
@@ -3,11 +3,13 @@ export class Users {
     username: string
     email: string
     password: string
+    file: number
 
     constructor() {
         this.id = 0
         this.username = ''
         this.email = ''
         this.password = ''
+        this.file = 1
     }
 }
