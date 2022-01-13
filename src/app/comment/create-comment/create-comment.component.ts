f4ce26e2e85b31b1075306f6dcd3541747db93a9
Ghazi <ghaziatie@gmail.com>
2022-01-13 11:13:10 +0100

    create component create comment

--- src/app/comment/create-comment/create-comment.component.ts
+++ src/app/comment/create-comment/create-comment.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from 'src/app/_models/Comment.model';
+import { Users } from 'src/app/_models/Users.model';
+import { CommentService } from 'src/app/_services/comment.service';
 
 @Component({
   selector: 'app-create-comment',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCommentComponent implements OnInit {
 
-  constructor() { }
+  commentForm: FormGroup | any
+  comment: Comment
+  @Input() user: Users
 
+  constructor(private formBuilder: FormBuilder, private commentService: CommentService) { this.comment = { id: 0, created:'', description:'', user: 0 }, this.user = { id: 0, username: '', password: '', email: '', file: 1 } }
   ngOnInit(): void {
+    this.initForm()
+  }
+
+  initForm() {
+    this.commentForm = this.formBuilder.group({
+      description: ['', Validators.required],
+    });
+  }
+
+  getComment() {
+    const description = this.commentForm?.get('description')?.value;
+    this.comment = new Comment()
+    this.comment.description = description
+    this.comment.user = this.user.id
+    console.log(this.comment)
+
+    this.commentService.createComment(this.comment).subscribe(data => {
+      console.log(data)
+      window.location.reload()
+    },
+    error => {
+      console.log(error)
+    })
   }
 
 }
