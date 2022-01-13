93c505a7057193dd7d4ae21fcac13710272fc4e8
Ghazi <ghaziatie@gmail.com>
2022-01-13 11:14:13 +0100

    configure edit recipe

---
+++ src/app/recipes/edit-recipe/edit-recipe.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from 'src/app/_models/Recipe.model';
+import { RecipesService } from 'src/app/_services/recipes.service';
+
+@Component({
+  selector: 'app-edit-recipe',
+  templateUrl: './edit-recipe.component.html',
+  styleUrls: ['./edit-recipe.component.css']
+})
+export class EditRecipeComponent implements OnInit {
+
+  recipe: Recipe;
+  id = this.route.snapshot.params['id']
+
+  constructor(private recipeService: RecipesService, private route: ActivatedRoute) { this.recipe = { id: 0, title: '', description: '', file: 0, category: 0, user: 0} }
+
+  ngOnInit(): void {
+    this.recipeService.getSingleRecipe(this.id).subscribe(data => {
+      this.recipe = data
+      console.log(data)
+    })
+  }
+
+}
