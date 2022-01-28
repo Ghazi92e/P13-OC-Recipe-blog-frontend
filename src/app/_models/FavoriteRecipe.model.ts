b0dec63b20e0f088a7341b7fd3e3b5fdffbd2270
Ghazi <ghaziatie@gmail.com>
2022-01-29 00:39:31 +0100

    create model FavoriteRecipe

---
+++ src/app/_models/FavoriteRecipe.model.ts
@@ -0,0 +1,11 @@
+export class FavoriteRecipe {
+    id: number
+    user: number
+    recipe: number
+
+    constructor() {
+        this.id = 0
+        this.user = 0
+        this.recipe = 0
+    }
+}
