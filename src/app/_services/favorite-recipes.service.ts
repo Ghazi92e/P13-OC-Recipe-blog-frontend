0d272124cbe768a62936421231dc5ecc9ce98029
Ghazi <ghaziatie@gmail.com>
2022-01-29 00:40:07 +0100

    create service favorite-recipes

---
+++ src/app/_services/favorite-recipes.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FavoriteRecipe } from '../_models/FavoriteRecipe.model';
+
+const url = 'http://127.0.0.1:8000/api-favoriterecipe/'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteRecipesService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllFavoriteRecipes() {
+    return this.http.get<FavoriteRecipe[]>(url)
+  }
+
+  createFavoriteRecipe(data: any) {
+    return this.http.post(url, data);
+  }
+
+  deleteFavoriteRecipe(id: any) {
+    return this.http.delete(`${url}${id}`);
+  }
+
+  // findByFavRecipe(recipe: any) {
+  //   return this.http.get<FavoriteRecipe[]>(`${url}?recipe=${recipe}`);
+  // }
+}
