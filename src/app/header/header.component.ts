8a1a0df8385f47b59f8d05dd9db0b5fd4c131c4c
Ghazi <ghaziatie@gmail.com>
2022-02-20 22:49:25 +0100

    create feature to search user followings recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { Users } from '../_models/Users.model';
+import { UsersService } from '../_services/users.service';
+import { Output, EventEmitter } from '@angular/core';
+import { RecipesService } from '../_services/recipes.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,8 +13,16 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   currentuser: any
+  getUserFollowing: any[] = []
+  currentFollowingUser: any
+  headerForm: FormGroup
+  user: Users
 
-  constructor(public router: Router) { }
+  recipesFollowingsUserHeader: any[] = []
+
+  display = false
+
+  constructor(public router: Router, private userService: UsersService, private formBuilder: FormBuilder, private recipeService: RecipesService) { this.headerForm = this.formBuilder.group({recipedata: ['', Validators.required]}), this.user = { id: 0, username: '', password: '', email: '', file: 0, image_url: '' } }
 
   ngOnInit(): void {
     this.currentuser = localStorage.getItem('token');
@@ -19,6 +31,14 @@ export class HeaderComponent implements OnInit {
     } else {
       console.log("je suis co")
     }
+
+    this.userService.getCurrentUser(this.currentuser).subscribe(data => {
+      this.user = data[0]
+      this.getFollowingCurrentUser(this.user.id)
+      console.log(this.user)
+    })
+
+    console.log(this.router.url)
   }
 
   signOut() {
@@ -28,5 +48,35 @@ export class HeaderComponent implements OnInit {
       window.location.reload()
     });
   }
+
+
+  getFollowingCurrentUser(currentuserid: any) {
+    console.log(currentuserid)
+    this.getUserFollowing.push(currentuserid)
+    this.userService.getFollowingUser(currentuserid).subscribe(data => {
+      this.currentFollowingUser = data
+
+      console.log(this.currentFollowingUser)
+      
+      for (let data of this.currentFollowingUser) {
+        this.getUserFollowing.push(data.user_following[0])
+      }
+      console.log(this.getUserFollowing)
+    })
+  }
+  
+
+  getHeader() {
+    console.log(this.getUserFollowing)
+    let recipedata = this.headerForm?.get('recipedata')?.value;
+    const UppercaseSearchbar = recipedata.charAt(0).toUpperCase() + recipedata.slice(1)
+    console.log(UppercaseSearchbar)
+    const obj = {
+      "title": UppercaseSearchbar,
+      "user__in": this.getUserFollowing
+    }
+    this.recipeService.subject.next(obj)
+  }
+
 
 }
