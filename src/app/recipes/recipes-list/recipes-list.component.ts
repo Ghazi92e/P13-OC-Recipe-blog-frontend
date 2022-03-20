24b053df9c3f7c95a931d5e7a1927ad9d224a9b5
Ghazi <ghaziatie@gmail.com>
2022-03-21 00:06:47 +0100

    refacto

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,18 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService } from '../../_services/users.service';
-import { Recipe } from '../../_models/Recipe.model';
 import { Users } from '../../_models/Users.model';
-import { RecipesService } from '../../_services/recipes.service';
-import { UploadfileService } from 'src/app/_services/uploadfile.service';
-import { FileUpload } from 'src/app/_models/Fileupload.model';
 import { FavoriteRecipe } from 'src/app/_models/FavoriteRecipe.model';
 import { FavoriteRecipesService } from 'src/app/_services/favorite-recipes.service';
 import { CategoryService } from 'src/app/_services/category.service';
 import { Category } from 'src/app/_models/Category.model';
-import { interval } from 'rxjs';
-import { HttpParams } from '@angular/common/http';
-
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
@@ -29,19 +22,17 @@ export class RecipesListComponent implements OnInit {
 
   dataEventCategories: number[] = []
 
-  currentFollowingUser: any
-
   recipesFollowingsUser: any[] = []
 
-  getUserFollowing: any[] = []
-
-  constructor(private recipesService: RecipesService, private userService: UsersService, private router: Router, private uploadFileService: UploadfileService, private favoriteRecipeService: FavoriteRecipesService, private categoryService: CategoryService) {
-    this.user = { id: 0, username: '', password: '', email: '', file: 0, image_url: '' }
+  constructor(private userService: UsersService, private router: Router, private favoriteRecipeService: FavoriteRecipesService, private categoryService: CategoryService) {
+    this.user = { id: 0, username: '', password: '', email: '', file: 0, image_url: '', is_superuser: false }
     this.addfavRecipe = { id: 0, user: 0, recipe: 0 }
   }
 
   ngOnInit(): void {
     this.currentuser = localStorage.getItem('token');
+    
+    console.log(this.currentuser)
     if (this.currentuser == null) {
       console.log("je suis deco")
       this.router.navigate(['sign-in'])
@@ -67,7 +58,6 @@ export class RecipesListComponent implements OnInit {
 
   getRecipeCategory(event: any) {
     this.p = 1
-    console.log(this.currentFollowingUser)
     const dataEvent = event.target.value
 
     this.recipesFollowingsUser = []
@@ -80,14 +70,12 @@ export class RecipesListComponent implements OnInit {
     }
 
     if (this.dataEventCategories.length == 0) {
-      this.getUserFollowing = []
       this.getFollowingCurrentUser(this.user.id)
     } else {
       const objectcat = {
         'category__in': this.dataEventCategories,
-        'user__in': this.getUserFollowing
       }
-      this.userService.getUserRecipesFollowing_by_category(objectcat).subscribe(data => {
+      this.userService.getUserRecipesFollowing_by_category(this.user.id, objectcat).subscribe(data => {
         this.recipesFollowingsUser = data
         console.log(data)
       })
@@ -106,27 +94,9 @@ export class RecipesListComponent implements OnInit {
   }
 
   getFollowingCurrentUser(currentuserid: any) {
-    console.log(currentuserid)
-    this.getUserFollowing.push(currentuserid)
-    this.userService.getFollowingUser(currentuserid).subscribe(data => {
-      this.currentFollowingUser = data
-      console.log(this.currentFollowingUser)
-
-      if (this.currentFollowingUser.length == 0) {
-        this.currentFollowingUser = [{'user_follower': currentuserid.toString(), 'user_following': '0'}]
-      }
-      console.log(this.currentFollowingUser)
-      this.userService.getUserFollowingRecipes(this.currentFollowingUser).subscribe(data => {
-        this.recipesFollowingsUser = data
-        console.log(data)
-      })
-
-      for (let data of this.currentFollowingUser) {
-        this.getUserFollowing.push(data.user_following[0])
-      }
-
-      this.userService.userFollowingsData.next(this.getUserFollowing)
+    this.userService.getUserFollowingRecipes(currentuserid).subscribe(data => {
+      this.recipesFollowingsUser = data
+      console.log(data)
     })
-
   }
 }
