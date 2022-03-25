58b7e62b68f9e1276e0f66128960963f3cbb8e56
Ghazi <ghaziatie@gmail.com>
2022-03-26 00:16:20 +0100

    refacto

--- src/app/http-interceptors/auth-interceptor.ts
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -10,21 +10,13 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private auth: UsersService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    // Get the auth token from the service.
+    // Get the user auth token from the service.
     const authToken = this.auth.getAuthorizationToken();
 
-    // Clone the request and replace the original headers with
-    // cloned headers, updated with the authorization.
-    if (req.url == 'http://127.0.0.1:8000/api-token-auth/') {
         this.authReq = req.clone({
-            headers: req.headers.set('Authorization', '')
+            headers: req.headers.set('Authorization', authToken ? 'Token ' + authToken: '')
         });
-    } else {
-        this.authReq = req.clone({
-            headers: req.headers.set('Authorization', 'Token ' + authToken)
-        });
-    }
-    // send cloned request with header to the next handler.
+
     return next.handle(this.authReq);
   }
 }
